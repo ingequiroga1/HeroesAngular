6197547dc1f997143aa1b2f2eb653bf3d0955398
ingequiroga <ingequiroga1@hotmail.com>
2019-12-17 18:55:18 -0600

    Leccion 59 Pipes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroes',
@@ -10,13 +12,19 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService:HeroesService ) {
+  constructor( private _heroesService:HeroesService,
+               private router:Router 
+               ) {
       console.log("constructor");
   }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log( this.heroes );
+  }
+
+  verHeroe( idx:number ){
+    this.router.navigate(['/heroe',idx]);
   }
 
 }
